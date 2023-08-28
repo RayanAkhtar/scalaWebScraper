3ce61f540b8606ddf4ab1f344dc8de3567dcf624
RayanAkhtar <rayanakhtar2003@gmail.com>
2023-08-28 17:51:00 +0100

    added another helper function and started working on profile creation

--- webscraper/src/main/scala/generalHelpers.scala
+++ webscraper/src/main/scala/generalHelpers.scala
@@ -11,9 +11,8 @@ object generalHelpers extends App {
    * @return Returns the number once an appropriate value has been entered
    */
   def takeNumberRange(lowerBound: Int, upperBound: Int): Int = {
-    var valid = false
     var choice = -1
-    while (!valid) {
+    while (true) {
       println(s"Please enter a number between $lowerBound and $upperBound:")
 
       val tryChoice = Try(scala.io.StdIn.readInt())
@@ -23,11 +22,30 @@ object generalHelpers extends App {
           println("Please only enter an integer")
           -1
       }
-      valid = choice >= lowerBound && choice <= upperBound
-
+      if (choice >= lowerBound && choice <= upperBound) return choice
     }
     choice
   }
 
+  /**
+   * Gets a response from the user when asking for a yes/no question
+   *
+   * @return returns whether the user said yes or no to the decision
+   */
+  def takeYesOrNo(): Boolean = {
+    while (true) {
+      println("Please enter [Y]es or [N]o: ")
+      val tryChoice = Try(scala.io.StdIn.readLine().trim)
+
+      val choice = tryChoice match {
+        case Success(value) => if (value.nonEmpty) value.charAt(0) else 'z'
+        case Failure(_) => 'z'
+      }
+
+      if (choice == 'y') return true
+      else if (choice == 'n') return false
+    }
+    false
+  }
 
 }
--- webscraper/src/main/scala/profile.scala
+++ webscraper/src/main/scala/profile.scala
@@ -1,3 +1,41 @@
+import java.io.File
+
 class profile {
+
+  def main(args: Array[String]): Unit = {
+
+    checkForProfileCreation()
+
+    println(
+      """
+        | 1. See user profile
+        | 2. Edit user profile
+        | 3. Delete user profile
+        | 4. Return to main menu
+        |""".stripMargin)
+    val choice = generalHelpers.takeNumberRange(1, 4)
+
+    choice match {
+      case 1 => println("Now going to see user profile") // todo implement first
+      case 2 => println("Now going edit user profile") // todo implement
+      case 3 => println("Now going to delete user profile") // todo implement
+      case 4 => return // redundant return but better off clarifying
+      case _ => println("Something went wrong here")
+    }
+
+  }
+
+  private def checkForProfileCreation(): Unit = {
+    val file = new File("/../userFiles/profile.txt").exists()
+    if (file) return
+
+    println("You have not created a file yet, would you like to create one now?")
+    val response = generalHelpers.takeYesOrNo()
+    if (!response) return
+
+    //todo have code here to create a file
+
+
+  }
 
 }
